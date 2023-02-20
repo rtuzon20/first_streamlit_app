be371985a2c162169d23462ad655dff118a51108
rtuzon20 <125537379+rtuzon20@users.noreply.github.com>
2023-02-20 14:18:40 +0800

    Update streamlit_app.py
    Normalized

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,3 +26,8 @@ streamlit.header('Fruityvice Fruit Advice')
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# set to normal -what does the next line do? 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# calling set variables and put it on a data frame - what does this do?
+streamlit.dataframe(fruityvice_normalized)
