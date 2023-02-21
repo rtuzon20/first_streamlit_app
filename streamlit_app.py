972f382174fe73fb7cb7dc9c7def5d2926c8e17a
rtuzon20 <125537379+rtuzon20@users.noreply.github.com>
2023-02-21 11:43:29 +0800

    Update streamlit_app.py
    typo error

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,4 +45,4 @@ streamlit.dataframe(my_data_rows)
 add_my_fruit=streamlit.text_input('What fruit would you like to add?','Jackfruit')
 streamlit.write('Thanks for adding ', add_my_fruit)
 
-my_cur.execute("insert into fruit_load_list values('streamlit.write(add_my_fruit)')")
+my_cur.execute("insert into fruit_load_list values('from streamlit')")
